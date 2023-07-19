86808f51d46400f8711038a7a0e15f37af405603
yogyy <yogimr261@gmail.com>
2023-07-19 20:36:41 +0700

    add CI and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react-todo-localhost/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
