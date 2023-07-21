5037c30be337aa78ee21d0dead960d6d3224490f
yogyy <yogimr261@gmail.com>
2023-07-21 23:32:45 +0700

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './index.css';
+import Root from './routes/root.tsx';
+import ErrorPage from './error-page.tsx';
+import Navbar from './components/layout/navbar.tsx';
 
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: '/',
+//       element: <App />,
+//       errorElement: <ErrorPage />,
+//     },
+//     {
+//       path: '/root',
+//       element: <Root />,
+//       errorElement: <ErrorPage />,
+//     },
+//   ],
+//   { basename: '/react-todo-localhost/' }
+// );
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navbar />} errorElement={<ErrorPage />}>
+      <Route index element={<App />} />
+      <Route path="/root" element={<Root />} />
+    </Route>
+  ),
+  { basename: '/react-todo-localhost/' }
+);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
