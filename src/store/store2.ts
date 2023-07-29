e8d18909d7838a1cccc9ff105c21b8e0bb8c987a
yogyy <yogimr261@gmail.com>
2023-07-29 23:57:40 +0700

    fix: move task store

--- src/store/store2.ts
+++ src/store/store2.ts
@@ -4,15 +4,18 @@ import { v1 as uuidv1 } from 'uuid';
 import { persist } from 'zustand/middleware';
 
 type Todos = {
-  planned: Task[];
-  ongoing: Task[];
-  done: Task[];
+  [category: string]: Task[];
 };
 
 type Store = {
   todos: Todos;
   addTodos: (title: string, state: TaskStatus) => void;
   deleteTodos: (id: string, state: TaskStatus) => void;
+  moveTaskBetweenCategories: (
+    taskId: string,
+    targetCategory: string,
+    targetIndex: number
+  ) => void;
 };
 // Define the store
 const useTodos = create<Store>()(
@@ -25,7 +28,7 @@ const useTodos = create<Store>()(
       },
       addTodos: (title, state) => {
         set(prev => {
-          const newTask = { title, status: state, id: uuidv1() };
+          const newTask = { title, state, id: uuidv1() };
           return {
             todos: {
               ...prev.todos,
@@ -44,6 +47,43 @@ const useTodos = create<Store>()(
           };
         });
       },
+      moveTaskBetweenCategories: (
+        taskId: string,
+        targetCategory: string,
+        targetIndex: number
+      ) =>
+        set((store: Store) => {
+          const { todos } = store;
+
+          const sourceCategory = Object.keys(todos).find(category =>
+            todos[category].some(task => task.id === taskId)
+          );
+
+          if (!sourceCategory || targetIndex < 0) {
+            // Invalid indices or categories, do nothing
+            return store; // Return the original store to avoid the error
+          }
+
+          const sourceTasks = todos[sourceCategory];
+          const taskToMoveIndex = sourceTasks.findIndex(
+            task => task.id === taskId
+          );
+
+          if (taskToMoveIndex === -1 || targetIndex > sourceTasks.length) {
+            // Invalid indices, do nothing
+            return store; // Return the original store to avoid the error
+          }
+
+          const taskToMove = sourceTasks.splice(taskToMoveIndex, 1)[0];
+
+          // Move the task to the target category and reset its order
+          todos[targetCategory].splice(targetIndex, 0, {
+            ...taskToMove,
+            state: targetCategory,
+          });
+
+          return { todos: { ...todos } };
+        }),
     }),
     { name: 'to-doooo' }
   )
