005863f5c38cb5f57245665f225b93a93e230616
yogyy <yogimr261@gmail.com>
2023-12-30 02:23:15 +0700

    feat: change modal from headless to radix(shadcn)

--- src/components/layout/modal.tsx
+++ src/components/layout/modal.tsx
@@ -1,60 +1,30 @@
-import { Fragment, ReactNode } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { modalState } from '@/store/todoStore';
 
 interface ModalProps {
-  children: ReactNode;
+  children: React.ReactNode;
+  description: React.ReactNode;
   title: string;
-  isOpen: boolean;
-  closeModal: () => void;
-  initialFocus: React.MutableRefObject<null>;
 }
 
-export default function Modal({
-  children,
-  title,
-  closeModal,
-  isOpen,
-  initialFocus,
-}: ModalProps) {
+export default function Modal({ children, description, title }: ModalProps) {
+  const { open, setOpen } = modalState();
   return (
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        onClose={closeModal}
-        initialFocus={initialFocus}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-300"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black bg-opacity-70" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-full p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform shadow-xl outline-secondary outline bg-bg rounded-2xl">
-                <Dialog.Title>{title}</Dialog.Title>
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription asChild>{description}</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
