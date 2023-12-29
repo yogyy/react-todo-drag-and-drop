d9e2dc9e68a852215c315c7995f3a4b3932f9c45
yogyy <yogimr261@gmail.com>
2023-12-30 02:17:47 +0700

    feat: add little animation 👌

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Column from '@/components/layout/column';
 import { DragDropContext, DropResult } from '@hello-pangea/dnd';
 import './App.css';
-import useTodos from './store/todoStore';
+import { useTodos } from './store/todoStore';
 
 function App() {
   const orderTask = useTodos(store => store.moveTaskBetweenCategories);
@@ -19,20 +19,20 @@ function App() {
   }
 
   return (
-    <>
-      <div className="flex flex-col items-center min-h-screen">
-        <div className="flex items-center justify-center w-full h-20 mb-12 bg-secondary">
-          <h1 className="text-3xl font-semibold">To DOoooooooooooo</h1>
-        </div>
-        <div className="flex flex-col items-center justify-center w-full md:flex-row md:items-start">
-          <DragDropContext onDragEnd={handleOnDragEnd}>
-            <Column variant="planned" state="planned" droppableId="planned" />
-            <Column variant="ongoing" state="ongoing" droppableId="ongoing" />
-            <Column variant="done" state="done" droppableId="done" />
-          </DragDropContext>
-        </div>
+    <div className="flex flex-col items-center min-h-screen">
+      <div className="flex items-center justify-center w-full h-20 mb-12 bg-secondary">
+        <h1 className="text-3xl font-semibold bg-gradient-to-r from-orange-500 via-sky-500 to-green-500 bg-clip-text text-transparent motion-safe:animate-gradient-x">
+          To DOoooooooooooo
+        </h1>
       </div>
-    </>
+      <div className="flex flex-col items-center justify-center w-full md:flex-row md:items-start">
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Column variant="planned" state="planned" droppableId="planned" />
+          <Column variant="ongoing" state="ongoing" droppableId="ongoing" />
+          <Column variant="done" state="done" droppableId="done" />
+        </DragDropContext>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,10 +32,21 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'gradient-x': 'gradient-x 3.5s ease infinite',
       },
     },
   },
