ee122ce4c664c5a9a58df11acc56db7c30715f0d
yogyy <yogimr261@gmail.com>
2024-02-14 00:16:12 +0700

    fix: fix modal

--- src/components/layout/modal.tsx
+++ src/components/layout/modal.tsx
@@ -5,8 +5,8 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
-import { modalState } from '@/store/todoStore';
+} from "@/components/ui/dialog";
+import { useState } from "react";
 
 interface ModalProps {
   children: React.ReactNode;
@@ -15,14 +15,16 @@ interface ModalProps {
 }
 
 export default function Modal({ children, description, title }: ModalProps) {
-  const { open, setOpen } = modalState();
+  const [open, setOpen] = useState(false);
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
-          <DialogDescription asChild>{description}</DialogDescription>
+          <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
