2ab1625f521fab340fc1e02221db38adca6939ea
yogyy <yogimr261@gmail.com>
2025-01-15 23:15:51 +0700

    fix: remove checkmark

--- src/components/task/task-order.tsx
+++ src/components/task/task-order.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ChecboxIcon } from "../icons/checkbox";
 import { cn } from "@/lib/utils";
 import {
   Tooltip,
@@ -10,37 +9,27 @@ import {
 import { TaskStatus } from "@/store/todoStore";
 import { CheckmarkIcon } from "../icons/checkmark";
 
-interface TaskOrderProps extends React.HTMLAttributes<HTMLDivElement> {
+interface TaskOrderProps {
   state: TaskStatus;
   order: number;
-  isDragging: boolean;
 }
-const Order = ({ state, isDragging, order, className }: TaskOrderProps) => {
+const Order = ({ state, order }: TaskOrderProps) => {
   return (
-    <div className={cn("flex items-center gap-2 mt-1.5 h-8", className)}>
-      <TooltipProvider delayDuration={150}>
-        <Tooltip>
-          <TooltipTrigger>
-            <ChecboxIcon />
-          </TooltipTrigger>
-          <TooltipContent>Task</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+    <div className="mt-1.5 flex h-8 w-full items-center justify-between gap-2">
       <TooltipProvider delayDuration={150}>
         <Tooltip>
           <TooltipTrigger
             className={cn(
-              "text-xs leading-4 relative group/kan inline-flex justify-center",
-              state === "done" ? "line-through decoration-2" : ""
-            )}>
+              "relative inline-flex justify-center text-[0.65rem] leading-4 text-primary/70",
+              state === "done" ? "line-through decoration-2" : "",
+            )}
+          >
             {`KAN-${order}`}
           </TooltipTrigger>
           <TooltipContent>{`KAN-${order}`}</TooltipContent>
         </Tooltip>
       </TooltipProvider>
-      {state === "done" && !isDragging && (
-        <CheckmarkIcon className="text-green-500 absolute right-0 -mt-1" />
-      )}
+      {state === "done" && <CheckmarkIcon className="text-green-500" />}
     </div>
   );
 };
