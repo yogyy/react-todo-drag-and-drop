225502b16e5b65a890ebf2dafc4e88a19ef2812c
yogyy <yogimr261@gmail.com>
2025-01-17 00:03:01 +0700

    chore: allow adding new tasks only to 'planned'

--- src/components/layout/column.tsx
+++ src/components/layout/column.tsx
@@ -20,24 +20,21 @@ interface ColumnProps
   variant: TaskVariant;
 }
 
+const columnTitle = (state: string): string => {
+  const titles: Record<string, string> = {
+    planned: "TO DO",
+    ongoing: "IN PROGRESS",
+    done: "DONE",
+  };
+
+  return titles[state] || "";
+};
+
 export const Column = React.forwardRef<HTMLDivElement, ColumnProps>(
   ({ variant, className }, ref) => {
     const addTodo = useTodos((store) => store.addTodos);
     const todos = useTodos((store) => store.todos[variant]);
-
-    const [editing, setEditing] = React.useState(false);
-    const title = (state: string): string => {
-      switch (state) {
-        case "planned":
-          return "TO DO";
-        case "ongoing":
-          return "IN PROGRESS";
-        case "done":
-          return "DONE";
-        default:
-          return ""; // or provide a default value
-      }
-    };
+    const [isAddingtodo, setIsAddingTodo] = React.useState(false);
 
     const form = useForm<z.infer<typeof formSchema>>({
       resolver: zodResolver(formSchema),
@@ -49,8 +46,9 @@ export const Column = React.forwardRef<HTMLDivElement, ColumnProps>(
     function onSubmit(values: z.infer<typeof formSchema>) {
       addTodo(values.task, variant);
       form.reset();
-      setEditing((prev) => !prev);
+      setIsAddingTodo((prev) => !prev);
     }
+
     return (
       <div
         ref={ref}
@@ -61,26 +59,28 @@ export const Column = React.forwardRef<HTMLDivElement, ColumnProps>(
       >
         <div className="flex items-center justify-between px-2 py-4">
           <div className="flex h-6 w-full items-center justify-between text-xs">
-            <Badge variant={variant} className="ml-2 font-bold">
-              <h1>• {title(variant)} •</h1>
+            <Badge variant={variant} className="ml-2 cursor-default font-bold">
+              <h1>{columnTitle(variant)}</h1>
             </Badge>
-            <button
-              className={cn(
-                "mt-0.5 items-center rounded-md bg-secondary p-1.5 text-sm font-semibold text-white/70",
-                "transition duration-300 group-hover/column:visible hover:cursor-pointer hover:bg-accent",
-                editing ? "hidden" : "flex",
-              )}
-              onClick={() => {
-                if (inputRef.current) {
-                  setEditing((prev) => !prev);
-                  setTimeout(() => {
-                    inputRef.current?.focus();
-                  }, 0);
-                }
-              }}
-            >
-              <PlusIcon className="h-5 w-5" />
-            </button>
+            {variant === "planned" ? (
+              <button
+                className={cn(
+                  "mt-0.5 items-center rounded-md bg-secondary p-1.5 text-sm font-semibold text-white/70",
+                  "transition duration-300 group-hover/column:visible hover:cursor-pointer hover:bg-accent",
+                  isAddingtodo ? "hidden" : "flex",
+                )}
+                onClick={() => {
+                  if (inputRef.current) {
+                    setIsAddingTodo((prev) => !prev);
+                    setTimeout(() => {
+                      inputRef.current?.focus();
+                    }, 0);
+                  }
+                }}
+              >
+                <PlusIcon className="h-5 w-5" />
+              </button>
+            ) : null}
           </div>
         </div>
         <Droppable droppableId={variant}>
@@ -103,46 +103,47 @@ export const Column = React.forwardRef<HTMLDivElement, ColumnProps>(
               ))}
               {provided.placeholder}
 
-              <div
-                className={cn(
-                  "border-2 border-biru p-2",
-                  editing === true ? "block" : "hidden",
-                )}
-              >
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="relative mt-0.5 rounded-sm"
-                    onBlur={() => setEditing(false)}
-                  >
-                    <FormField
-                      control={form.control}
-                      name="task"
-                      render={({ field }) => (
-                        <FormItem className="relative text-bg">
-                          <FormControl ref={inputRef}>
-                            <textarea
-                              maxLength={100}
-                              onKeyDown={(e) => {
-                                if (e.key === "Enter") {
-                                  e.preventDefault();
-                                  form.handleSubmit(onSubmit)();
-                                }
-                                if (e.key === "Escape") {
-                                  setEditing(false);
-                                }
-                              }}
-                              className="flex min-h-10 w-full resize-none justify-start overflow-y-scroll rounded-sm border-none bg-bg text-[14px] text-primary outline-none scrollbar-none placeholder:text-[14px]"
-                              placeholder="What needs to be done?"
-                              {...field}
-                            />
-                          </FormControl>
-                        </FormItem>
-                      )}
-                    />
-                  </form>
-                </Form>
-              </div>
+              {variant === "planned" ? (
+                <div
+                  className={cn(
+                    "border-2 border-biru p-2",
+                    isAddingtodo === true ? "block" : "hidden",
+                  )}
+                >
+                  <Form {...form}>
+                    <form
+                      onSubmit={form.handleSubmit(onSubmit)}
+                      className="relative mt-0.5 rounded-sm"
+                      onBlur={() => setIsAddingTodo(false)}
+                    >
+                      <FormField
+                        control={form.control}
+                        name="task"
+                        render={({ field }) => (
+                          <FormItem className="relative text-bg">
+                            <FormControl ref={inputRef}>
+                              <textarea
+                                maxLength={100}
+                                onKeyDown={(e) => {
+                                  if (e.key === "Enter") {
+                                    form.handleSubmit(onSubmit)();
+                                  }
+                                  if (e.key === "Escape") {
+                                    setIsAddingTodo(false);
+                                  }
+                                }}
+                                className="flex min-h-10 w-full resize-none justify-start overflow-y-scroll rounded-sm border-none bg-bg text-[14px] text-primary outline-none scrollbar-none placeholder:text-[14px]"
+                                placeholder="What needs to be done?"
+                                {...field}
+                              />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                    </form>
+                  </Form>
+                </div>
+              ) : null}
             </div>
           )}
         </Droppable>
