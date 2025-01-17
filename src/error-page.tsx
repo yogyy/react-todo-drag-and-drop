c74218a94066978b4e818a9def5b6c49c376d2f7
yogyy <yogimr261@gmail.com>
2025-01-17 17:26:51 +0700

    chroe: add red color to error

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
@@ -6,9 +6,9 @@ export default function ErrorPage() {
   return (
     <div
       id="error-page"
-      className="h-screen w-screen flex justify-center items-center flex-col"
+      className="flex h-screen w-screen flex-col items-center justify-center"
     >
-      <h1 className="text-2xl">Oops!</h1>
+      <h1 className="text-2xl text-red-600">Oops!</h1>
       <p className="text-xl">
         <i>{error.status}</i> <br />
         <i>{error.data}</i>
